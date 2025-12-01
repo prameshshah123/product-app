dc1b4f9febf24aa019b240a6703013283e4a516e
prameshshah123 <cpackprint@gmail.com>
2025-12-01 16:37:24 +0530

    Format index.js for improved readability

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import { useEffect } from 'react'; import { useRouter } from 'next/router'; export default function Home(){ const r=useRouter(); useEffect(() => {
-  const go = async () => {
-    await r.replace('/products');
-  };
-  go();
-}, [r]); return null }
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const r = useRouter();
+
+  useEffect(() => {
+    const go = async () => {
+      await r.replace("/products");
+    };
+    go();
+  }, [r]);
+
+  return null;
+}
