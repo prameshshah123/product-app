e68c73543feec33c0ec0b01f21a04021f423e3a7
prameshshah123 <cpackprint@gmail.com>
2025-12-01 16:36:32 +0530

    Refactor useEffect to use async function for routing

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,6 @@
-import { useEffect } from 'react'; import { useRouter } from 'next/router'; export default function Home(){ const r=useRouter(); useEffect(()=>r.replace('/products'),[]); return null }
+import { useEffect } from 'react'; import { useRouter } from 'next/router'; export default function Home(){ const r=useRouter(); useEffect(() => {
+  const go = async () => {
+    await r.replace('/products');
+  };
+  go();
+}, [r]); return null }
